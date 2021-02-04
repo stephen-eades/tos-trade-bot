660f32ffd615c0c7f5a0e253c77b2ef62b23e021
stephen.eades <stephen.a.eades@gmail.com>
2021-02-04 00:31:28 -0500

    Bot now Tweets positions each morning, adjusted schedule

--- clock.py
+++ clock.py
@@ -3,11 +3,11 @@ from tweet_trade import tweet_trades_from_prior_day, tweet_positions
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='tue-sat', hour='6', minute="00", timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='tue-sat', hour='6', minute="01", timezone='America/New_York')
 def tweet_trades_from_prior_day_scheduled_job():
     tweet_trades_from_prior_day()
 
-@sched.scheduled_job('cron', day_of_week='mon', hour='5', minute="55", timezone='America/New_York')
+@sched.scheduled_job('cron', day_of_week='mon', hour='6', minute="05", timezone='America/New_York')
 def tweet_positions_scheduled_job():
     tweet_positions()
 
